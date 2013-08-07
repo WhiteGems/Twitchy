a56fb97317608f3af014eb1a52728dc0ccc72f52
Zesty <zestybaby@126.com>
2013-08-07 23:48:03 +0800

    Revert "一波带走所有文本的汉化"
    This reverts commit 25bbcbe9eb568a904219aedaaa9e500a3227c3dc.

--- src/us/compressions/twitchy/Stream.java
+++ src/us/compressions/twitchy/Stream.java
@@ -16,7 +16,7 @@ public class Stream implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
         if(!(sender instanceof Player)) {
-            sender.sendMessage(ChatColor.RED + "你必须是一名玩家才能执行这个指令!");
+            sender.sendMessage(ChatColor.RED + "You must be a player to execute this command!");
         } else {
             if(label.equalsIgnoreCase("stream")) {
                 Player player = (Player) sender;
@@ -24,11 +24,11 @@ public class Stream implements CommandExecutor {
                 if(plugin.isOnline()) {
                     String online = plugin.getConfig().getString("onlineStatus");
                     player.sendMessage(ChatColor.translateAlternateColorCodes('&', online));
-                    player.sendMessage(ChatColor.GREEN + "直播频道链接: " + ChatColor.DARK_AQUA + "http://twitch.tv/" + channel);
+                    player.sendMessage(ChatColor.GREEN + "Channel Link: " + ChatColor.DARK_AQUA + "http://twitch.tv/" + channel);
                 } else {
                     String offline = plugin.getConfig().getString("offlineStatus");
                     player.sendMessage(ChatColor.translateAlternateColorCodes('&', offline));
-                    player.sendMessage(ChatColor.GREEN + "直播频道链接: " + ChatColor.DARK_AQUA + "http://twitch.tv/" + channel);
+                    player.sendMessage(ChatColor.GREEN + "Channel Link: " + ChatColor.DARK_AQUA + "http://twitch.tv/" + channel);
                 }
             }
         }
